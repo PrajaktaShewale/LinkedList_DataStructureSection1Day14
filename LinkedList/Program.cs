92ea615ba4aeff528ffc7b2428f9804a11b66e06
PrajaktaShewale <shewalep94@gmail.com>
2022-12-19 17:43:03 +0530

    [Prajakta]Add:Simple Linked List

---
+++ LinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleLinkedList list = new SimpleLinkedList();
+            list.Addnote(56);
+            list.Addnote(30);
+            list.Addnote(70);
+            list.Display();
+
+        }
+    }
+}
